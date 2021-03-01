71ffcdc6646488d510a8fdce1f4d4f334e6b64d3
SashaShlemkevich <sashashlemcevich@gmail.com>
2021-03-01 11:21:45 +0200

    fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -57,8 +57,19 @@
 </main>
 <hr class="featurette-divider">
 <footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="float-end"><a href="#">^</a></p>
-        <p>©2021</p>
+    <div class="container">
+    <h3 style="color: black">Контакти</h3>
+    <p style="color: black">Будемо раді відповісти  на ваші запитання</p>
+    <input type="text" name="name" id="name" placeholder="Ім'я">
+    <input type="text" name="phone" id="phone" placeholder="Телефон">
+    <input type="email" name="email" id="email" placeholder="Пошта"><br>
+    <textarea style="margin: 10px" name="massage" placeholder="Опис вина"></textarea><br>
+    <button type="submit" class="btn btn-danger">Відправити</button>
+    </div>
+    <a style="float:right; padding: 15px; color: black;" href="#"><svg style="width: 25px; height: 25px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354
+            7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+        </svg></a>
 </footer>
 </body>
 </html>
